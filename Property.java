1781a6508aa49e9e5e0f171b6c40ba401c9e358f
TaxiDave8 <75022309+TaxiDave8@users.noreply.github.com>
2020-12-01 02:16:06 +0000

    Update Property.java
    I have amended this, tax and the management menu in order to split up the tax and property classes therefore making the code more coherent

--- Property.java
+++ Property.java
@@ -1,8 +1,5 @@
-import java.util.Random;
+import java.util.Scanner;
 
-/**
- * if weve time at the end, fuck tax stuff into it's own class
- */
 public class Property {
 
     private String ownerName;
@@ -11,24 +8,28 @@ public class Property {
     private double marketValue;
     private int location;
     private boolean ppr;
-
-    protected double taxDue;
-    protected int yearsOverDue;
-    protected double taxOverDue;
+    private Tax tax;
 
 
     public Property(String ownerName, String address, String postCode,
-                    double marketValue, int location, boolean ppr, int yOD) {
+                    double marketValue, int location, boolean ppr, Tax tax) {
         this.ownerName = ownerName;
         this.address = address;
         this.postCode = postCode;
         this.marketValue = marketValue;
         this.location = location;
         this.ppr = ppr;
+        this.tax = tax;
+    }
 
-        this.yearsOverDue = yOD;
-        this.taxDue = this.getTaxDue();
-        this.taxOverDue = this.getTaxOverDue();
+    public Property(String ownerName, String address, String postCode,
+                    double marketValue, int location, boolean ppr) {
+        this.ownerName = ownerName;
+        this.address = address;
+        this.postCode = postCode;
+        this.marketValue = marketValue;
+        this.location = location;
+        this.ppr = ppr;
     }
 
     public String getOwner() {
@@ -39,7 +40,8 @@ public class Property {
         return postCode;
     }
 
-    public double getMarketValue() { return marketValue;
+    public double getMarketValue() {
+        return marketValue;
     }
 
     public int getLocation() {
@@ -50,65 +52,49 @@ public class Property {
         return ppr;
     }
 
-    public double getTaxDue() {
-        double thisYearsTax = this.calcAnnualTax();
-        if( yearsOverDue != 0 ){
-            thisYearsTax = thisYearsTax * Math.pow(1.07, yearsOverDue );
-        }
-        return thisYearsTax;
-    }
-
-    public double calcAnnualTax() {
-        double annualTax = 100;
-
-        if ( marketValue <= 150000) {
-            annualTax += 0;
-        } else if (marketValue > 150000 && marketValue <= 400000) {
-            annualTax += marketValue * .0001;
-        } else if (marketValue > 400000 && marketValue <= 650000) {
-            annualTax += marketValue * .0002;
-        } else if (marketValue > 650000) {
-            annualTax += marketValue * .0004;
-        }
-
-        switch (location) {
-            case 0: annualTax += 100;
-                break;
-            case 1: annualTax += 80;
-                break;
-            case 2: annualTax += 60;
-                break;
-            case 3: annualTax += 50;
-                break;
-            case 4: annualTax += 25;
-                break;
-        }
-
-        if ( ppr ) {
-            annualTax += 100;
-        }
-        return annualTax;
-    }
-
-    public double getTaxOverDue(){
-        double overDue = 0;
-        if ( yearsOverDue == 0){
-            return 0;
+    public void payTaxDue(){
+        if(tax.taxDue == 0){
+            System.out.println("You're tax is all paid up on this property");
+        }else if (tax.taxOverDue != 0){
+            System.out.println("Press 0 if you would like to just pay your overdue tax and 1 if you wish to pay all due tax.");
+            Scanner keyboard = new Scanner( System.in);
+            int choice = keyboard.nextInt();
+            switch(choice){
+                case 0: System.out.println("You have paid $" + tax.taxOverDue + " worth of overdue tax on this property");
+                        tax.taxOverDue = 0;
+                        break;
+                case 1: System.out.println("You have paid $" + tax.taxOverDue + " worth of overdue tax on this property");
+                        System.out.println("You have also paid $" + tax.taxDue + " worth of tax due on this property");
+                        tax.taxOverDue = 0;
+                        tax.taxDue = 0;
+            }
         } else {
-            overDue = this.calcAnnualTax() * ( 1 - Math.pow( 1.07, yearsOverDue) )/ ( 1 - 1.07 );
+            System.out.println("You have paid $" + tax.taxDue + " worth of tax on this property");
+            tax.taxDue = 0;
         }
-        return overDue;
     }
 
-    public double payTaxDue(){
-        taxDue = 0;
-        return taxDue;
+    public String locationToString(){
+        String s = "";
+        switch (location){
+            case 0: s+= "City";
+                break;
+            case 1: s+= "Large Town";
+                break;
+            case 2: s+= "Small Town";
+                break;
+            case 3: s+= "Village";
+                break;
+            case 4: s+= "Countryside";
+                break;
+
+        }
+        return s;
     }
 
     public String toString(){
-        String s = "Address: " + address + ", Post Code: " + postCode +
-                ", Market Value: " + marketValue + ", Location: " +
-                location + ", Principal Private Residence? " + ppr  + ", Tax Due: " + taxDue +  ", Tax OverDue: " + taxOverDue;
+        String s = "Address: " + address + ", Post Code: " + postCode + ", Market Value: " + marketValue + "," +
+                " Location: " + this.locationToString() + ", Principal Private Residence? " + ppr + tax.toString();
         return s;
     }
 }
