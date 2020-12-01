063e0389a1ba3d1c1a8519a8152f3d54736297b6
TaxiDave8 <75022309+TaxiDave8@users.noreply.github.com>
2020-12-01 22:47:56 +0000

    Update PropertyOwnerMenu.java
    Making the menu compatible with the seperate tax and property classes

--- PropertyOwnerMenu.java
+++ PropertyOwnerMenu.java
@@ -2,59 +2,62 @@ import java.util.Scanner;
 
 public class PropertyOwnerMenu {
 
-    private final Scanner keyboard;
+    private Scanner keyboard;
 
     public PropertyOwnerMenu(){
         keyboard = new Scanner(System.in);
     }
 
     public void run(){
+        boolean more = true;
         System.out.println("Owner's name: ");
         String ownerName = keyboard.nextLine();
         Owner owner = new Owner(ownerName);
 
-        while (true){
+        while (more){
             System.out.println("A)dd Properties, S)how Properties P)ay Tax");
             String command = keyboard.nextLine().toUpperCase();
 
-            switch (command) {
-                case "A":
-                    System.out.println("Address: ");
-                    String address = keyboard.nextLine();
-                    System.out.println("PostCode: ");
-                    String postCode = keyboard.nextLine();
-                    System.out.println("Market Value: ");
-                    double marketValue = keyboard.nextDouble();
-                    System.out.println("Location Category 0- City, 1- Large Town, 2- Small Town: 3- Village, 4- Countryside");
-                    int locationCategory = keyboard.nextInt();
-                    System.out.println("Is this your principal private residence?: Enter 1 if it is and enter 0 if it isn'nt");
-                    boolean ppr = false;
-                    if (keyboard.nextInt() == 1) {
-                        ppr = true;
-                    }
-                    System.out.println("Years of overdue tax? ");
-                    int yearsOfOverDueTax = keyboard.nextInt();
-                    owner.addProperty(new Property(owner.getName(), address, postCode, marketValue, locationCategory, ppr, yearsOfOverDueTax));
+            if(command.equals("A")){
+                System.out.println("Address: ");
+                String address = keyboard.nextLine();
+                System.out.println("PostCode: ");
+                String postCode = keyboard.nextLine();
+                System.out.println("Market Value: ");
+                double marketValue = keyboard.nextDouble();
+                System.out.println("Location Category 0- City, 1- Large Town, 2- Small Town: 3- Village, 4- Countryside");
+                int locationCategory = keyboard.nextInt();
+                System.out.println("Is this your principal private residence?: Enter 1 if it is and enter 0 if it isn'nt");
+                boolean ppr = false;
+                if (keyboard.nextInt() == 1){
+                    ppr = true;
+                }
+                System.out.println("Years of overdue tax? ");
+                int yearsOfOverDueTax = keyboard.nextInt();
+                Tax tax = new Tax(yearsOfOverDueTax);
+                tax.addTheProperty( new Property(owner.getName(), address, postCode, marketValue, locationCategory, ppr ) );
+                owner.addProperty( new Property(owner.getName(), address, postCode, marketValue, locationCategory, ppr, tax ));
 
-                    break;
-                case "S":
-                    for (Property p : owner.getProperties()) {
-                        System.out.println(p.toString());
-                    }
-                    break;
-                case "P":
-                    System.out.print("Choose property from Postcode: " + "\n");
-                    for (Property p : owner.getProperties()) {
-                        System.out.println(p.getPostCode());
-                    }
-                    String choice = keyboard.nextLine();
-                    for (Property p : owner.getProperties()) {
-                        if (choice.equalsIgnoreCase(p.getPostCode())) {
-                            p.payTaxDue();
-                        }
-                    }
+            }
 
-                    break;
+            else if (command.equals("S")){
+                for(Property p: owner.getProperties()){
+                    System.out.println(p.toString());
+                }
+            }
+
+            else if(command.equals("P")){
+                System.out.print("Choose property from Postcode: " + "\n");
+                for(Property p: owner.getProperties()) {
+                    System.out.println(p.getPostCode());
+                }
+                String choice = keyboard.nextLine();
+                for (Property p: owner.getProperties()){
+                    if(choice.equalsIgnoreCase( p.getPostCode() ) ){
+                        p.payTaxDue();
+                    }
+                }
+
             }
 
         }
