2aa916ed1ac0a8534dcbcb732cb1aa46230ff55d
TaxiDave8 <75022309+TaxiDave8@users.noreply.github.com>
2020-12-03 13:50:06 +0000

    Update BalancingStatement.java
    2 new getter methods, 1 new data field and a much nicer toString

--- BalancingStatement.java
+++ BalancingStatement.java
@@ -2,25 +2,40 @@ import java.util.ArrayList;
 
 public class BalancingStatement {
 
-    int year;
-    double taxDue;
-    ArrayList<Payment> payments;
+    private int year;
+    private double taxDueForYear;
+    private double taxPaidThisYear;
+    private ArrayList<Payment> payments;
 
-    public BalancingStatement(int year){
+    public BalancingStatement(int year, double taxDueForYear){
         this.year = year;
+        this.taxDueForYear = taxDueForYear;
         this.payments = new ArrayList<Payment>();
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public ArrayList<Payment> getPayments() {
+        return payments;
     }
 
     public void addPayment(Payment payment){
         payments.add(payment);
+        taxPaidThisYear += payment.getAmount();
     }
 
     @Override
     public String toString() {
-        String s = "BalancingStatement for " + "Year:" + year + "\n Payments:";
-        for(Payment p: payments){
-            s += p.toString() + "\n";
-        }
+        String s = "BalancingStatement for " + "Year:" + year + "\n Tax Due at 1/1: " + taxDueForYear + "\n                                " +
+                "Payments made\n                                ---------------\n";
+                for (Payment p: payments){
+                    s += "                                " + p.toString() + "\n";
+                }
+                s += "\n                                -------------------------- \n";
+                s += "                                Total Tax Paid: " + taxPaidThisYear +
+                        "\n" + "Tax Due at 31/12: " + (taxDueForYear - taxPaidThisYear) + "\n\n";
         return s;
     }
 }
