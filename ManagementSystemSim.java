221ab1fc87beaad57e34d70fcc0112385c0d1ac6
TaxiDave8 <75022309+TaxiDave8@users.noreply.github.com>
2020-12-03 15:49:40 +0000

    Update ManagementSystemSim.java
    Updating the sim class so that can run either the Dept's menu or an owners menu depending on user input

--- ManagementSystemSim.java
+++ ManagementSystemSim.java
@@ -1,8 +1,19 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class ManagementSystemSim {
 
     public static void main(String[] args ){
-        PropertyOwnerMenu menu = new PropertyOwnerMenu();
-        menu.run();
+        Scanner keyboard = new Scanner( System.in);
+        PropertyOwnerMenu ownerMenu = new PropertyOwnerMenu();
+        DeptOfEnvironmentMenu deptMenu = new DeptOfEnvironmentMenu();
 
+        System.out.println("Are you a 1) Property Owner or a 2) Dept of Environment Manager");
+        int menuChoice = keyboard.nextInt();
+        if( menuChoice == 1){
+            ownerMenu.run();
+        } else if (menuChoice == 2){
+            deptMenu.run();
+        }
     }
 }
